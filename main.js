029a8d2479ee0b008e4c71d63ed352c296361014
karan singh <93322323+karan9551@users.noreply.github.com>
2022-07-18 18:27:57 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const toggle_nav=document.querySelector(".toggler");
-const nav_header=document.querySelector(".header");
-
-const toggleNavbar =()=>{
-    nav_header.classList.toggle('active')
-}
-
-toggle_nav.addEventListener('click',()=>toggleNavbar());
